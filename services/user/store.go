38a6f21bbb0f9ed3aac09ad309fea04beea72589
dunky-star <dunkygeoffrey39@gmail.com>
2024-07-10 23:41:47 -0500

    Implementing repository layer - Native SQL

--- services/user/store.go
+++ services/user/store.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/dunky-star/ecomm-proj/payloads"
 )
@@ -21,4 +22,68 @@ func (s *Store) CreateUser(user payloads.User) error {
 	}
 
 	return nil
+}
+
+func (s *Store) GetUserByEmail(email string) (*payloads.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(payloads.User)
+	for rows.Next() {
+		u, err = scanRowsIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return u, nil
+}
+
+
+func (s *Store) GetUserByID(id int) (*payloads.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(payloads.User)
+	for rows.Next() {
+		u, err = scanRowsIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return u, nil
+}
+
+
+
+
+func scanRowsIntoUser(rows *sql.Rows) (*payloads.User, error) {
+	user := new(payloads.User)
+
+	err := rows.Scan(
+		&user.ID,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
