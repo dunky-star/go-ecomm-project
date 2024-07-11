b7e8aa530845e482190f79eb5c413ec7c6cc313b
dunky-star <dunkygeoffrey39@gmail.com>
2024-07-10 23:35:31 -0500

    Implementing repository layer - Native SQL

---
+++ services/user/store.go
@@ -0,0 +1,24 @@
+package user
+
+import (
+	"database/sql"
+
+	"github.com/dunky-star/ecomm-proj/payloads"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) CreateUser(user payloads.User) error {
+	_, err := s.db.Exec("INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)", user.FirstName, user.LastName, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
